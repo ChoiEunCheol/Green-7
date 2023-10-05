d4f48c17ff22be64e1e051518ee0cb8ad7dfb6aa
ChoiEunCheol <ChoiEunCheol>
2023-10-05 11:02:17 +0900

    어제 한글이 깨졌던 이유 = 문서 형식으로 안만들어서

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const http = require("http");
+
+const server = http.createServer(function(request, response){
+    // console.log(typeof(request)); //object
+    // console.log(typeof(response)); //object
+    response.writeHead(200,{"Content-Type":"text/html; charset=utf-8"})
+    
+    let doc = `<html><head><body><h1> 오 된다 </h1></body></head></html>`
+
+    response.end(doc);
+});
+
+server.listen(1234);
