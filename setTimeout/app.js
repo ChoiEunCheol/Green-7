1a673153e0ec704e0dcf99f77330ae8b6d4ef124
ChoiEunCheol <142868888+ChoiEunCheol@users.noreply.github.com>
2023-10-06 13:11:41 +0900

    Add files via upload

---
+++ setTimeout/app.js
@@ -0,0 +1,32 @@
+setInterval(function(){},1000)
+
+// function sosa() {
+//     setTimeout(function(){
+//         console.log("ㅇㅇ");
+//         sosa();
+//     },1000)
+// }
+
+// setTimeout(function () {
+//   console.log("1");
+
+//   setTimeout(function () {
+//     console.log("2");
+
+//     setTimeout(function () {
+//         console.log("3");
+//       }, 1000);
+
+//   }, 2000);
+
+// }, 3000);
+
+
+// setTimeout(function () {
+//   console.log("1");
+// }, 1000.99999999999999);
+// // 9 : 14개부터 3이 먼저 찍힘
+
+// setTimeout(function () {
+//   console.log("3");
+// }, 1000);
